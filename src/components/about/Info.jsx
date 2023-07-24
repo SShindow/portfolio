ce90db1586b5bd657e17c78a92623e1eb05c495a
Hai Cao Xuan <hia.cao.2001@gmail.com>
2023-07-25 01:37:36 +0700

    about-section

---
+++ src/components/about/Info.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Info = () => {
+    return (
+        <div className="about__info grid">
+            <div className="about__box">
+            <i class="bx bx-briefcase-alt about__icon"></i>
+                <h3 className="about__title">Experience</h3>
+                <span className="about__subtitle">3+ years </span>
+            </div>
+
+            <div className="about__box">
+            <i class='bx bx-award about__icon' ></i>
+                <h3 className="about__title about__icon">Completed</h3>
+                <span className="about__subtitle">5 Projects</span>
+            </div>
+
+            <div className="about__box">
+            <i class='bx bx-time about__icon'></i>
+                <h3 className="about__title about__icon">Service</h3>
+                <span className="about__subtitle">Office hours</span>
+            </div>
+        </div>
+    )
+}
+
+export default Info;
