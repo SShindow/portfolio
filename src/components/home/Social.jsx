96f71de91faaad05e196205694bc764bb58f5855
Hai Cao Xuan <hia.cao.2001@gmail.com>
2023-07-24 14:02:35 +0700

    home-profile

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Social = () => {
+    return (
+        <div className="home__social">
+            <a href="https://www.facebook.com/hiacao1811/" 
+                className="home__social-icon" target="_blank">
+                <i class="uil uil-facebook-f"></i>
+            </a>
+            <a href="https://www.instagram.com/hiacao_1811/?hl=en" 
+                className="home__social-icon" target="_blank">
+                <i class="uil uil-instagram"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/haicaoxuan/"
+                className="home__social-icon" target="_blank">
+                <i class="uil uil-linkedin"></i>
+            </a>    
+            <a href="https://github.com/SShindow" 
+                className="home__social-icon" target="_blank">
+                <i class="uil uil-github"></i>
+            </a>
+            
+        </div>
+    )
+}
+
+export default Social;
