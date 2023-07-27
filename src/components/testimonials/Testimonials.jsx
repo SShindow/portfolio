7f9507c6e39ae47072da23143cb5d959fc624963
Hai Cao Xuan <hia.cao.2001@gmail.com>
2023-07-27 22:47:04 +0700

    scroll-fix

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const Testimonials = () => {
   return (
-    <div>Testimonials</div>
-  )
-}
+    <section className="testimonials container section">
+      <h2 className="section__title">My clients say</h2>
+      <span className="section__subtitle">Testimonials</span>
+    </section>
+  );
+};
 
-export default Testimonials
+export default Testimonials;
