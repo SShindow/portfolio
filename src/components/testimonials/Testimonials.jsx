0c6229d1c6cf31e8b41c055a6758983d65c8950a
Hai Cao Xuan <hia.cao.2001@gmail.com>
2023-07-27 23:03:33 +0700

    home-fix

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import "./testimonials.css";
+import Data from "./Data";
 
 const Testimonials = () => {
   return (
     <section className="testimonials container section">
-      <h2 className="section__title">My clients say</h2>
-      <span className="section__subtitle">Testimonials</span>
+      <h2 className="section__title">Testimonials</h2>
+      <span className="section__subtitle">My clients says</span>
+
+      <div className="testimonials__container">
+
+      </div>
     </section>
   );
 };
