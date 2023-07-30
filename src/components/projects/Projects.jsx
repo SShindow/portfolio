76032f45f11edd5b5e15a5785b713ebf3306690a
Hai Cao Xuan <hia.cao.2001@gmail.com>
2023-07-30 14:47:12 +0700

    projects template

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import './projects.css';
+import Works from './Works';
 
 const Projects = () => {
   return (
-    <section className="project container section" id='projects'>
+    <section className="project section" id='projects'>
         <h2 className="section__title">Projects</h2>
         <span className="section__subtitle">My recent works</span>
-    </section>
-  )
-}
 
-export default Projects
+        <Works/>
+    </section>
+  );
+};
+
+export default Projects;
