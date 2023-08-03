103b75a7b533da57273ef1fb2c5b666842194458
Hai Cao Xuan <hia.cao.2001@gmail.com>
2023-08-03 13:59:26 +0700

    projects data update

--- src/components/projects/Data.jsx
+++ src/components/projects/Data.jsx
@@ -1,39 +1,46 @@
-import Work1 from "../../assets/work1.jpg";
-import Work2 from "../../assets/work2.jpg";
-import Work3 from "../../assets/work3.jpg";
-import Work4 from "../../assets/work4.jpg";
-import Work5 from "../../assets/work5.jpg";
+import Work1 from "../../assets/work1.png";
+import Work2 from "../../assets/work2.png";
+import Work3 from "../../assets/work3.png";
+import Work4 from "../../assets/work4.png";
+import Work5 from "../../assets/work5.png";
+import Work6 from "../../assets/work6.png";
 
 export const projectsData = [
     {
         id: 1,
         image: Work1,
-        title: "Web design",
+        title: "Shopee Clone",
         category: "web",
     },
     {
         id: 2,
         image: Work2,
-        title: "App movil",
+        title: "MediMe",
         category: "app",
     },
     {
         id: 3,
         image: Work3,
-        title: "Brand design",
+        title: "MediMe Design",
         category: "design",
     },
     {
         id: 4,
         image: Work4,
-        title: "App movil",
+        title: "MÜSS",
         category: "app",
     },
     {
         id: 5,
         image: Work5,
-        title: "Web design",
-        category: "web",
+        title: "EHealth",
+        category: "app",
+    },
+    {
+        id: 6,
+        image: Work6,
+        title: "MÜSS Design",
+        category: "design",
     },
 ];
 
